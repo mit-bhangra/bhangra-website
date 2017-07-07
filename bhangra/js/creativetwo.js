e7a26eb44d128bb4b2cb1ec309d08ee32736d514
pkarnati2004 <pkarnati@mit.edu>
2017-07-07 12:10:17 +0100

    changing site due to issues (1) with contact form

--- bhangra/js/creativetwo.js
+++ bhangra/js/creativetwo.js
@@ -63,75 +63,75 @@
 
 })(jQuery); // End of use strict
 
-//adapted from website design templates done by Erlen (erlen.co.uk)
+// //adapted from website design templates done by Erlen (erlen.co.uk)
 
-// Auto resize input
-function resizeInput() {
-    $(this).attr('size', $(this).val().length);
-}
+// // Auto resize input
+// function resizeInput() {
+//     $(this).attr('size', $(this).val().length);
+// }
 
-$('input[type="text"], input[type="email"]')
-    // event handler
-    .keyup(resizeInput)
-    // resize on page load
-    .each(resizeInput);
+// $('input[type="text"], input[type="email"]')
+//     // event handler
+//     .keyup(resizeInput)
+//     // resize on page load
+//     .each(resizeInput);
 
 
-console.clear();
-// Adapted from georgepapadakis.me/demo/expanding-textarea.html
-(function(){
+// console.clear();
+// // Adapted from georgepapadakis.me/demo/expanding-textarea.html
+// (function(){
   
-  var textareas = document.querySelectorAll('.expanding'),
+//   var textareas = document.querySelectorAll('.expanding'),
       
-      resize = function(t) {
-        t.style.height = 'auto';
-        t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
-        t.style.height = (t.scrollHeight + t.offset ) + 'px';
-        t.style.overflow = '';
-      },
+//       resize = function(t) {
+//         t.style.height = 'auto';
+//         t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
+//         t.style.height = (t.scrollHeight + t.offset ) + 'px';
+//         t.style.overflow = '';
+//       },
       
-      attachResize = function(t) {
-        if ( t ) {
-          console.log('t.className',t.className);
-          t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
+//       attachResize = function(t) {
+//         if ( t ) {
+//           console.log('t.className',t.className);
+//           t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
 
-          resize(t);
+//           resize(t);
 
-          if ( t.addEventListener ) {
-            t.addEventListener('input', function() { resize(t); });
-            t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
-          }
+//           if ( t.addEventListener ) {
+//             t.addEventListener('input', function() { resize(t); });
+//             t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
+//           }
 
-          t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
-        }
-      };
+//           t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
+//         }
+//       };
   
-  // IE7 support
-  if ( !document.querySelectorAll ) {
+//   // IE7 support
+//   if ( !document.querySelectorAll ) {
   
-    function getElementsByClass(searchClass,node,tag) {
-      var classElements = new Array();
-      node = node || document;
-      tag = tag || '*';
-      var els = node.getElementsByTagName(tag);
-      var elsLen = els.length;
-      var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
-      for (i = 0, j = 0; i < elsLen; i++) {
-        if ( pattern.test(els[i].className) ) {
-          classElements[j] = els[i];
-          j++;
-        }
-      }
-      return classElements;
-    }
+//     function getElementsByClass(searchClass,node,tag) {
+//       var classElements = new Array();
+//       node = node || document;
+//       tag = tag || '*';
+//       var els = node.getElementsByTagName(tag);
+//       var elsLen = els.length;
+//       var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+//       for (i = 0, j = 0; i < elsLen; i++) {
+//         if ( pattern.test(els[i].className) ) {
+//           classElements[j] = els[i];
+//           j++;
+//         }
+//       }
+//       return classElements;
+//     }
     
-    textareas = getElementsByClass('expanding');
-  }
+//     textareas = getElementsByClass('expanding');
+//   }
   
-  for (var i = 0; i < textareas.length; i++ ) {
-    attachResize(textareas[i]);
-  }
+//   for (var i = 0; i < textareas.length; i++ ) {
+//     attachResize(textareas[i]);
+//   }
   
-})();
+// })();
 
 
